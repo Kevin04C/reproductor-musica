0a9f8fae7bdbabd8fb5000bca6e1d2c8fc8d1468
Kevin <kevincespedes2004@gmail.com>
2022-07-23 17:51:24 -0500

    añadiendo mejoras

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 import './normalize.css'
 import './style.css'
 
+import data from './songs.json'
+
+
+const $ = selector => document.querySelector(selector)
+const audio = new Audio();
+
+const btnPause = $('#btn-pause')
+const btnPlay = $('#btn-play')
+const indexAudio = 0;
+
+  
+
+
+
+
 
 
